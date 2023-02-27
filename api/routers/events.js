47556c2a1c3a4b4d9025ef368ad5c6fc63c8d53f
Theresa Grimus <T.Grimus@gmx.net>
2023-02-27 11:13:59 +0100

    added querys for date to and from

--- api/routers/events.js
+++ api/routers/events.js
@@ -3,13 +3,64 @@ import fs from "fs";
 const eventsRouter = express.Router();
 
 eventsRouter.get("/", (req, res) => {
-  const events = JSON.parse(fs.readFileSync("./data/events.json"));
+  let events = JSON.parse(fs.readFileSync("./data/events.json"));
+  if (req.query["from"] !== undefined) {
+    let date;
+    if (req.query["from"] === "now") {
+      const dateOfToday = new Date();
+      let dayOfToday =
+        dateOfToday.getDate() < 10
+          ? `0${dateOfToday.getDate()}`
+          : `${dateOfToday.getDate()}`;
+      let monthOfToday =
+        dateOfToday.getMonth() + 1 < 10
+          ? `0${dateOfToday.getMonth() + 1}`
+          : `${dateOfToday.getMonth() + 1}`;
+      date = `${dateOfToday.getFullYear()}-${monthOfToday}-${dayOfToday}`;
+    } else {
+      date = req.query["from"];
+    }
+    events = events.filter((event) => {
+      let dayOfEvent;
+      let monthOfEvent;
+      dayOfEvent =
+        event.date.day < 10 ? `0${event.date.day}` : `${event.date.day}`;
+      monthOfEvent =
+        event.date.month < 10 ? `0${event.date.month}` : `${event.date.month}`;
+
+      let dateOfEvent = `${event.date.year}-${monthOfEvent}-${dayOfEvent}`;
+      if (dateOfEvent >= date) {
+        return event;
+      }
+    });
+  }
+
+  if (req.query["to"] !== undefined) {
+    let date = req.query["to"];
+    console.log(req.query["to"]);
+    events = events.filter((event) => {
+      let dayOfEvent;
+      let monthOfEvent;
+      dayOfEvent =
+        event.date.day < 10 ? `0${event.date.day}` : `${event.date.day}`;
+      monthOfEvent =
+        event.date.month < 10 ? `0${event.date.month}` : `${event.date.month}`;
+
+      let dateOfEvent = `${event.date.year}-${monthOfEvent}-${dayOfEvent}`;
+      if (dateOfEvent <= date) {
+        return event;
+      }
+    });
+  }
+
+
+
   res.json(events);
 });
 
 eventsRouter.get("/:id", (req, res) => {
   const events = JSON.parse(fs.readFileSync("./data/events.json"));
-  const eventId = events.filter((event) => event.id.toString() === req.params.id);
+  const eventId = events.find((event) => event.id.toString() === req.params.id);
   res.json(eventId);
 });
 
